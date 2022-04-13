31b2aa5c8549c591a2b8dcf89c34187115592065
Joao Victor <jov9051@outlook.com>
2022-04-13 15:36:29 -0300

    feat: dbConnection definido em database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,12 @@
 import sequelize from 'sequelize';
+
+let { Sequelize } = sequelize;
+
+let dbConnection = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
