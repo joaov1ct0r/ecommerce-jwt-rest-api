7a73632c74e2c565499a93a74a7b8dccfabca5e1
Joao Victor <jov9051@outlook.com>
2022-04-13 15:52:45 -0300

    feat: modelo de usuario definido em userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,3 +1,28 @@
 import dbConnection from '../config/database.js';
 
 import { DataTypes } from 'sequelize';
+
+let User = dbConnection.define(
+    'users',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        freezeTableName: true,
+        tableName: 'users',
+        timestamps: false
+    }
+);
