b251a60d18cf2d9b6cb697f110eb78b84fbf5c58
Joao Victor <jov9051@outlook.com>
2022-04-13 16:04:34 -0300

    feat: modelo de produto criado em productModel.js

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,3 +1,36 @@
 import dbConnection from '../config/database.js';
 
 import { DataTypes } from 'sequelize';
+
+let Product = dbConnection.define(
+    'products',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        freezeTableName: true,
+        tableName: 'products',
+        timestamps: false
+    }
+);
