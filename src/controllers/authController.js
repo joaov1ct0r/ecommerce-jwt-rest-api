cca2d96d5b17d802b2315fbc886564675ed59435
Joao Victor <jov9051@outlook.com>
2022-04-14 19:27:38 -0300

    feat: validacao de auth ocorrendo em authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,7 @@
 import jwt from 'jsonwebtoken';
+
+export default function (req, res, next) {
+    let { auth } = req.cookies;
+
+    if (!auth) return res.status(400).json({ error: 'Token não encontrado!' });
+}
