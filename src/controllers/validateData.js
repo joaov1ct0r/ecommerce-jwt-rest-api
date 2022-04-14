fbe929fd2dc23951715223ac3f90dccadc373f94
Joao Victor <jov9051@outlook.com>
2022-04-14 18:15:04 -0300

    fea: loginValidate criado em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -8,4 +8,12 @@ let registerValidate = data => {
     return schema.validate(data);
 };
 
+let loginValidate = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250)
+    });
+    return schema.validate(data);
+};
+
 export { registerValidate };
