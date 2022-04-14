8b7eb8a7e88331107159d67cfba38ce07860cb43
Joao Victor <jov9051@outlook.com>
2022-04-14 18:24:17 -0300

    feat: validacao de dados em handleNewUser em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,3 +5,9 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 import { registerValidate, loginValidate } from './validateData.js';
+
+let handleNewUser = (req, res) => {
+    let { error } = registerValidate(req.body);
+
+    if (error) return res.status(400).json({ error });
+};
