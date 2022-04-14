fa8173443eeb8b1a75cc345c4ec0ec26c010de91
Joao Victor <jov9051@outlook.com>
2022-04-14 18:12:35 -0300

    feat: registerValidate criado em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -1,1 +1,9 @@
 import Joi from '@hapi/joi';
+
+let registerValidate = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250)
+    });
+    return schema.validate(data);
+};
