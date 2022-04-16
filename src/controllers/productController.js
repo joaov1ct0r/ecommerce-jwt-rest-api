42e2e7b4d3facbbd54ee6b41a46c20ddd755d634
Joao Victor <jov9051@outlook.com>
2022-04-16 15:52:50 -0300

    feat: validacao de usuario ocorrendo em handleNewProduct em productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,11 +5,18 @@ import Product from '../models/productModel.js';
 import { validateProductData } from './validateProductData.js';
 
 let handleNewProduct = async (req, res) => {
+    let { id } = req.params;
+
+    if (!id) return res.status(400).json({ error: 'ID não encontrado!' });
+
     let { error } = validateProductData(req.body);
 
     if (error) return res.status(400).json({ error });
 
-    let { id } = req.params;
+    let user = await User.findOne({
+        where: { id }
+    });
 
-    if (!id) return res.status(400).json({ error: 'ID não encontrado!' });
+    if (!user)
+        return res.status(400).json({ error: 'Usuario não encontrado!' });
 };
