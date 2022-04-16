8a9c5eb69d071f26c2a722fe8d5cb814d0c37341
Joao Victor <jov9051@outlook.com>
2022-04-16 15:43:40 -0300

    feat: validacao de dados de produto ocorrendo em productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,3 +3,9 @@ import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
 
 import { validateProductData } from './validateProductData.js';
+
+let handleNewProduct = async (req, res) => {
+    let { error } = validateProductData(req.body);
+
+    if (error) return res.status(400).json({ error });
+};
