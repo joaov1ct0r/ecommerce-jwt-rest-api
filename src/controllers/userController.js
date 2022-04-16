841a2db32254682207b9aa16731feb4994cf989b
Joao Victor <jov9051@outlook.com>
2022-04-16 16:04:29 -0300

    fix: codigo nao utilizado removido

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,9 +47,6 @@ let handleUserLogin = async (req, res) => {
 
     let { email, password } = req.body;
 
-    if (!email || !password)
-        return res.status(400).json({ error: 'Falha na autenticação!' });
-
     let selectedUser = await User.findOne({
         where: { email }
     });
