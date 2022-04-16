b12c060acc3e906a9beed05ff3dea3a6ed168450
Joao Victor <jov9051@outlook.com>
2022-04-16 15:38:38 -0300

    feat: validacao de dados de produto criado em validateProductData.js

--- src/controllers/validateProductData.js
+++ src/controllers/validateProductData.js
@@ -1,1 +1,11 @@
 import Joi from '@hapi/joi';
+
+let validateProductData = data => {
+    let schema = Joi.object({
+        title: Joi.string().required().min(3).max(150),
+        description: Joi.string().required().min(5).max(150),
+        amount: Joi.number().required().min(1),
+        price: Joi.number().required().min(1)
+    });
+    return schema.validate(data);
+};
