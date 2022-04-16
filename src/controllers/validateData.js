f228ccd3bc4a14f0fe82ef2a5f7b5298c07bd49c
Joao Victor <jov9051@outlook.com>
2022-04-16 15:23:40 -0300

    feat: editValidate criado em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -16,4 +16,12 @@ let loginValidate = data => {
     return schema.validate(data);
 };
 
+let editValidate = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250)
+    });
+    return schema.validate(data);
+};
+
 export { registerValidate, loginValidate };
