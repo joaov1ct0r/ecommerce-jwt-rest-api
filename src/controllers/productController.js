87f85eb94634b97c47ca29d55f689449db33829f
Joao Victor <jov9051@outlook.com>
2022-04-19 13:31:22 -0300

    feat: validateProductData importado para productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,7 @@ import User from '../models/userModel.js';
 
 import Product from '../models/productModel.js';
 
-import { validateProductData } from './validateProductData.js';
+import validateProductData from './validateProductData.js';
 
 let handleNewProduct = async (req, res) => {
     let { id } = req.params;
