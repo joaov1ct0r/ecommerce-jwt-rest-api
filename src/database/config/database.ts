6f814bfd5a020f23f3a347401400bcc1a2b30ba8
Joao Victor <jov9051@outlook.com>
2022-05-28 20:15:39 -0300

    refactor: database.js renomeado para database.ts

---
+++ src/database/config/database.ts
@@ -0,0 +1,22 @@
+import sequelize from 'sequelize';
+
+let { Sequelize } = sequelize;
+
+let dbConnection = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+});
+
+try {
+  await dbConnection.authenticate();
+
+  console.log('Conexão estabelecida com DB!');
+} catch (error) {
+  throw error;
+}
+
+export default dbConnection;
