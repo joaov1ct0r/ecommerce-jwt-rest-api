bf58a3b439c38d58ca4d9fa5797bf350d0393b55
Joao Victor <jov9051@outlook.com>
2022-05-28 20:16:35 -0300

    refactor: database.ts tipado

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,22 +1,9 @@
-import sequelize from 'sequelize';
+import sequelize from "sequelize";
 
-let { Sequelize } = sequelize;
-
-let dbConnection = new Sequelize({
-  dialect: 'mysql',
+const DB: sequelize.Sequelize = new sequelize.Sequelize(process.env.DB_DATABASE as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
+  dialect: "mysql",
   host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
+  port: Number(process.env.DB_PORT)
 });
 
-try {
-  await dbConnection.authenticate();
-
-  console.log('Conexão estabelecida com DB!');
-} catch (error) {
-  throw error;
-}
-
-export default dbConnection;
+export default DB;
