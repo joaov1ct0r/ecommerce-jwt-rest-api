5f432da7dbdf5f88302e26d7853eaa9a0fdffba9
Joao Victor <jov9051@outlook.com>
2022-05-28 22:49:34 -0300

    refactor: DB importado para productModel.ts

--- src/database/models/productModel.ts
+++ src/database/models/productModel.ts
@@ -1,49 +1,49 @@
-import dbConnection from '../config/database.js';
+import DB from "../config/database";
 
 import { DataTypes } from 'sequelize';
 
 import User from './userModel.js';
 
 let Product = dbConnection.define(
-    'products',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING(150),
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING(150),
-            allowNull: false
-        },
-        amount: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
+  'products',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        freezeTableName: true,
-        tableName: 'products',
-        timestamps: false
+    title: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING(150),
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
+  },
+  {
+    freezeTableName: true,
+    tableName: 'products',
+    timestamps: false
+  }
 );
 
 Product.belongsTo(User, {
-    constraints: true,
-    foreignKey: 'userId'
+  constraints: true,
+  foreignKey: 'userId'
 });
 
 User.hasMany(Product, {
-    foreignKey: 'userId'
+  foreignKey: 'userId'
 });
 
 Product.sync();
