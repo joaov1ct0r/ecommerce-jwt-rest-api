58f0353dfb4737da4f339a2068412ee6a0f0aa7a
Joao Victor <jov9051@outlook.com>
2022-05-29 20:24:54 -0300

    refactor: validateHandleNewUser tipado em validateUserData.ts

--- src/validators/validateUserData.ts
+++ src/validators/validateUserData.ts
@@ -1,11 +1,11 @@
 import Joi from "@hapi/joi";
 
-let validateUserData = data => {
-  let schema = Joi.object({
+const validateHandleNewUser = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
     email: Joi.string().required().min(10).max(100),
     password: Joi.string().required().min(8).max(250)
   });
   return schema.validate(data);
 };
 
-export default validateUserData;
+export default validateHandleNewUser;
