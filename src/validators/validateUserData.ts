2ad8c0326422b99f54a297bc28c987aa98a8d4b7
Joao Victor <jov9051@outlook.com>
2022-05-29 20:26:59 -0300

    refactor: validateHandleUserLogin tipado em validateUserData.ts

--- src/validators/validateUserData.ts
+++ src/validators/validateUserData.ts
@@ -8,4 +8,13 @@ const validateHandleNewUser = (data: Object): Joi.ValidationResult => {
   return schema.validate(data);
 };
 
-export default validateHandleNewUser;
+const validateHandleUserLogin = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    password: Joi.string().required().min(8).max(250)
+  });
+
+  return schema.validate(data);
+};
+
+export { validateHandleNewUser, validateHandleUserLogin };
