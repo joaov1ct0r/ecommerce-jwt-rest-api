2e3083d95507ebe376a9427cd1ccbb5df192a19f
Joao Victor <jov9051@outlook.com>
2022-05-29 20:33:35 -0300

    refactor: Joi importado para validateProductData.ts

--- src/validators/validateProductData.ts
+++ src/validators/validateProductData.ts
@@ -1,13 +1,13 @@
-import Joi from '@hapi/joi';
+import Joi from "@hapi/joi";
 
 let validateProductData = data => {
-    let schema = Joi.object({
-        title: Joi.string().required().min(3).max(150),
-        description: Joi.string().required().min(5).max(150),
-        amount: Joi.number().required().min(1),
-        price: Joi.number().required().min(1)
-    });
-    return schema.validate(data);
+  let schema = Joi.object({
+    title: Joi.string().required().min(3).max(150),
+    description: Joi.string().required().min(5).max(150),
+    amount: Joi.number().required().min(1),
+    price: Joi.number().required().min(1)
+  });
+  return schema.validate(data);
 };
 
 export default validateProductData;
