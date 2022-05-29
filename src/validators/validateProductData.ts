29987757eb9e1581410c37750a89de6b3b9ea68e
Joao Victor <jov9051@outlook.com>
2022-05-29 20:36:59 -0300

    refactor: validateHandlEditProduct tipado em validateProductData.ts

--- src/validators/validateProductData.ts
+++ src/validators/validateProductData.ts
@@ -10,4 +10,15 @@ const validateHandleNewProduct = (data: Object): Joi.ValidationResult => {
   return schema.validate(data);
 };
 
-export { validateHandleNewProduct };
+const validateHandleEditProduct = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    title: Joi.string().required().min(3).max(150),
+    description: Joi.string().required().min(5).max(150),
+    amount: Joi.number().required().min(1),
+    price: Joi.number().required().min(1)
+  });
+
+  return schema.validate(data);
+};
+
+export { validateHandleNewProduct, validateHandleEditProduct };
