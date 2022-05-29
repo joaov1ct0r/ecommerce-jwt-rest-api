b33da1b36196af6461c9918f1cb4d45053577da4
Joao Victor <jov9051@outlook.com>
2022-05-29 20:29:05 -0300

    refactor: validateHandleEditUser tipado em validateUserData.ts

--- src/validators/validateUserData.ts
+++ src/validators/validateUserData.ts
@@ -17,4 +17,12 @@ const validateHandleUserLogin = (data: Object): Joi.ValidationResult => {
   return schema.validate(data);
 };
 
-export { validateHandleNewUser, validateHandleUserLogin };
+const validateHandleEditUser = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    password: Joi.string().required().min(8).max(250)
+  });
+  return schema.validate(data);
+};
+
+export { validateHandleNewUser, validateHandleUserLogin, validateHandleEditUser };
