353788555a82043fa0757eb261ba3ba77d9fd759
Joao Victor <jov9051@outlook.com>
2022-05-29 20:21:44 -0300

    build: @types/hapi__joi instlado

--- src/validators/validateUserData.ts
+++ src/validators/validateUserData.ts
@@ -1,11 +1,11 @@
-import Joi from '@hapi/joi';
+import Joi from "@hapi/joi";
 
 let validateUserData = data => {
-    let schema = Joi.object({
-        email: Joi.string().required().min(10).max(100),
-        password: Joi.string().required().min(8).max(250)
-    });
-    return schema.validate(data);
+  let schema = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    password: Joi.string().required().min(8).max(250)
+  });
+  return schema.validate(data);
 };
 
 export default validateUserData;
