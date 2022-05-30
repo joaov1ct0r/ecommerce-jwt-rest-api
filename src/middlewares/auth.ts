bdbb5b713f52c81b642f99dccf24e65eb261f8d3
Joao Victor <jov9051@outlook.com>
2022-05-30 15:54:50 -0300

    refactor: jwt importado para auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,20 +1,20 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export default function (req, res, next) {
-    let { auth } = req.cookies;
+  let { auth } = req.cookies;
 
-    if (!auth) return res.status(400).json({ error: 'Token não encontrado!' });
+  if (!auth) return res.status(400).json({ error: 'Token não encontrado!' });
 
-    try {
-        let userVerified = jwt.verify(auth, process.env.JWT_TOKEN_SECRET);
+  try {
+    let userVerified = jwt.verify(auth, process.env.JWT_TOKEN_SECRET);
 
-        if (!userVerified)
-            return res
-                .status(400)
-                .json({ error: 'Falha na autenticação de token!' });
+    if (!userVerified)
+      return res
+        .status(400)
+        .json({ error: 'Falha na autenticação de token!' });
 
-        next();
-    } catch (error) {
-        throw error;
-    }
+    next();
+  } catch (error) {
+    throw error;
+  }
 }
