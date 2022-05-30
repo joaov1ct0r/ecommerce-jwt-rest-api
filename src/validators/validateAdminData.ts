196cd6b38f11c9bf96bce5864d8c5c1ab457744a
Joao Victor <jov9051@outlook.com>
2022-05-29 21:09:16 -0300

    feat: handleAdminEditUser exportado em validateAdminData.ts

--- src/validators/validateAdminData.ts
+++ src/validators/validateAdminData.ts
@@ -1,1 +1,13 @@
 import Joi from "@hapi/joi";
+
+const validateHandleAdminEditUser = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    userEmail: Joi.string().required().min(10).max(100),
+    userNewEmail: Joi.string().required().min(10).max(100),
+    userNewPassword: Joi.string().required().min(8).max(250)
+  });
+
+  return schema.validate(data);
+};
+
+export { validateHandleAdminEditUser };
