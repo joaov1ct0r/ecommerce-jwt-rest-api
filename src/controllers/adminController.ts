622dadf0a8b11074daca753f6a945952971d5fc3
Joao Victor <jov9051@outlook.com>
2022-05-31 17:08:05 -0300

    feat: handleAdminEditUser criado em adminController.ts

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -11,3 +11,39 @@ import { validateHandleAdminDeleteProduct, validateHandleAdminDeleteUser, valida
 import IUser from "../types/userInterface";
 
 import { Model } from "sequelize";
+
+const handleAdminEditUser = async (req: Request, res: Response) => {
+  const { error } = validateHandleAdminEditUser(req.body);
+
+  if (error) return res.status(400).json({ error });
+
+  const userEmail: string = req.body.userEmail;
+
+  const userNewEmail: string = req.body.userNewEmail;
+
+  const userNewPassword: string = req.body.userNewPassword;
+
+  try {
+    const isUserRegistered: IUser | null = await User.findOne({
+      where: { email: userEmail }
+    });
+
+    if (isUserRegistered === null) {
+      return res.status(404).json({ error: "Usuario não encontrado!" });
+    }
+
+    const editedUser: [affectedCount: number] = await User.update(
+      {
+        userNewEmail, password: bcrypt.hashSync(userNewPassword)
+      },
+      { where: { email: userEmail } });
+
+    if (editedUser[0] === 0) {
+      return res.status(500).json({ error: "Falha ao atualizar usuario!" });
+    };
+
+    return res.status(204).send();
+  } catch (err: unknown) {
+    return res.status(500).json({ err });
+  };
+};
