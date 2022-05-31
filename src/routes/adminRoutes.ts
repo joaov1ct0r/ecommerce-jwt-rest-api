f9b385b20df677b700404bc36deb65ba6921b983
Joao Victor <jov9051@outlook.com>
2022-05-31 17:27:37 -0300

    feat: auth importado para adminRoutes.ts

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,3 +1,5 @@
 import express from "express";
 
-import { handleAdminDeleteProduct, handleAdminDeleteUser, handleAdminEditUser } from "../controllers/adminController"; 
+import { handleAdminDeleteProduct, handleAdminDeleteUser, handleAdminEditUser } from "../controllers/adminController";
+
+import auth from "../middlewares/auth";
