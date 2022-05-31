ae3ab7b69fe299023366b579fe7bcb34a0ed3b04
Joao Victor <jov9051@outlook.com>
2022-05-31 16:05:39 -0300

    refactor: User importado para productController.ts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,157 +1,157 @@
-import User from '../database/models/userModel.js';
+import User from "../database/models/userModel";
 
 import Product from '../database/models/productModel.js';
 
 import validateProductData from '../validators/validateProductData.js';
 
 let handleNewProduct = async (req, res) => {
-    let { id } = req.params;
+  let { id } = req.params;
 
-    let { error } = validateProductData(req.body);
+  let { error } = validateProductData(req.body);
 
-    if (error) return res.status(400).json({ error });
+  if (error) return res.status(400).json({ error });
 
-    let registeredUser = await User.findOne({
-        where: { id }
+  let registeredUser = await User.findOne({
+    where: { id }
+  });
+
+  if (!registeredUser)
+    return res.status(400).json({ error: 'Usuario não encontrado!' });
+
+  let { title, description, amount, price } = req.body;
+
+  try {
+    let newProduct = await Product.create({
+      title,
+      description,
+      amount,
+      price,
+      userId: registeredUser.id
     });
 
-    if (!registeredUser)
-        return res.status(400).json({ error: 'Usuario não encontrado!' });
+    if (!newProduct)
+      return res
+        .status(500)
+        .json({ error: 'Falha ao registrar novo produto!' });
 
-    let { title, description, amount, price } = req.body;
-
-    try {
-        let newProduct = await Product.create({
-            title,
-            description,
-            amount,
-            price,
-            userId: registeredUser.id
-        });
-
-        if (!newProduct)
-            return res
-                .status(500)
-                .json({ error: 'Falha ao registrar novo produto!' });
-
-        res.status(200).json({ newProduct });
-    } catch (error) {
-        throw error;
-    }
+    res.status(200).json({ newProduct });
+  } catch (error) {
+    throw error;
+  }
 };
 
 let handleEditProduct = async (req, res) => {
-    let { id, productId } = req.params;
+  let { id, productId } = req.params;
 
-    let { error } = validateProductData(req.body);
+  let { error } = validateProductData(req.body);
 
-    if (error) return res.status(400).json({ error });
+  if (error) return res.status(400).json({ error });
 
-    let registeredUser = await User.findOne({
-        where: { id }
-    });
+  let registeredUser = await User.findOne({
+    where: { id }
+  });
 
-    if (!registeredUser)
-        return res.status(400).json({ error: 'Usuario não encontrado!' });
+  if (!registeredUser)
+    return res.status(400).json({ error: 'Usuario não encontrado!' });
 
-    let { title, description, amount, price } = req.body;
+  let { title, description, amount, price } = req.body;
 
-    try {
-        let editedProduct = await Product.update(
-            { title, description, amount, price, userId: registeredUser.id },
-            {
-                where: { id: productId }
-            }
-        );
+  try {
+    let editedProduct = await Product.update(
+      { title, description, amount, price, userId: registeredUser.id },
+      {
+        where: { id: productId }
+      }
+    );
 
-        if (!editedProduct)
-            return res.status(500).json({ error: 'Falha ao editar produto!' });
+    if (!editedProduct)
+      return res.status(500).json({ error: 'Falha ao editar produto!' });
 
-        res.status(200).json({ message: 'Produto editado com sucesso!' });
-    } catch (error) {
-        throw error;
-    }
+    res.status(200).json({ message: 'Produto editado com sucesso!' });
+  } catch (error) {
+    throw error;
+  }
 };
 
 let handleDeleteProduct = async (req, res) => {
-    let { id, productId } = req.params;
+  let { id, productId } = req.params;
 
-    let registeredUser = await User.findOne({
-        where: { id }
+  let registeredUser = await User.findOne({
+    where: { id }
+  });
+
+  if (!registeredUser)
+    return res.status(400).json({ error: 'Usuario não encontrado!' });
+
+  let registeredProduct = await Product.findOne({
+    where: { id: productId }
+  });
+
+  if (!registeredProduct)
+    return res.status(400).json({ error: 'Produto não encontrado!' });
+
+  try {
+    let deletedProduct = await Product.destroy({
+      where: { id: productId }
     });
 
-    if (!registeredUser)
-        return res.status(400).json({ error: 'Usuario não encontrado!' });
+    if (!deletedProduct)
+      return res.status(500).json({ error: 'Falha ao deletar produto!' });
 
-    let registeredProduct = await Product.findOne({
-        where: { id: productId }
-    });
-
-    if (!registeredProduct)
-        return res.status(400).json({ error: 'Produto não encontrado!' });
-
-    try {
-        let deletedProduct = await Product.destroy({
-            where: { id: productId }
-        });
-
-        if (!deletedProduct)
-            return res.status(500).json({ error: 'Falha ao deletar produto!' });
-
-        res.status(200).json({ message: 'Produto deletado com sucesso!' });
-    } catch (error) {
-        throw error;
-    }
+    res.status(200).json({ message: 'Produto deletado com sucesso!' });
+  } catch (error) {
+    throw error;
+  }
 };
 
 let handleGetAllProducts = async (req, res) => {
-    try {
-        let products = await Product.findAll({ include: User });
+  try {
+    let products = await Product.findAll({ include: User });
 
-        if (!products)
-            return res.status(500).json({ error: 'Falha ao obter dados!' });
+    if (!products)
+      return res.status(500).json({ error: 'Falha ao obter dados!' });
 
-        res.status(200).json({ products });
-    } catch (error) {
-        throw error;
-    }
+    res.status(200).json({ products });
+  } catch (error) {
+    throw error;
+  }
 };
 
 let handleGetOneProduct = async (req, res) => {
-    let { id, productId } = req.params;
+  let { id, productId } = req.params;
 
-    let registeredUser = await User.findOne({
-        where: { id }
-    });
+  let registeredUser = await User.findOne({
+    where: { id }
+  });
 
-    if (!registeredUser)
-        return res.status(400).json({ error: 'Usuario não encontrado!' });
+  if (!registeredUser)
+    return res.status(400).json({ error: 'Usuario não encontrado!' });
 
-    let registeredProduct = await Product.findOne({
-        where: { id: productId }
-    });
+  let registeredProduct = await Product.findOne({
+    where: { id: productId }
+  });
 
-    if (!registeredProduct)
-        return res.status(400).json({ error: 'Produto não encontrado!' });
+  if (!registeredProduct)
+    return res.status(400).json({ error: 'Produto não encontrado!' });
 
-    try {
-        let product = await Product.findByPk(productId, { include: User });
+  try {
+    let product = await Product.findByPk(productId, { include: User });
 
-        if (!product)
-            return res
-                .status(500)
-                .json({ error: 'Falha ao obter dados do produto!' });
+    if (!product)
+      return res
+        .status(500)
+        .json({ error: 'Falha ao obter dados do produto!' });
 
-        res.status(200).json({ product });
-    } catch (error) {
-        throw error;
-    }
+    res.status(200).json({ product });
+  } catch (error) {
+    throw error;
+  }
 };
 
 export {
-    handleNewProduct,
-    handleEditProduct,
-    handleDeleteProduct,
-    handleGetOneProduct,
-    handleGetAllProducts
+  handleNewProduct,
+  handleEditProduct,
+  handleDeleteProduct,
+  handleGetOneProduct,
+  handleGetAllProducts
 };
