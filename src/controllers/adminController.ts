80e173d1d2b6ef4d526c14a53e136501c5a0206b
Joao Victor <jov9051@outlook.com>
2022-05-31 16:57:19 -0300

    feat: Req e res importados para adminController.ts

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -3,3 +3,5 @@ import User from "../database/models/userModel";
 import Product from "../database/models/productModel";
 
 import bcrypt from "bcryptjs";
+
+import { Request, Response } from "express";
