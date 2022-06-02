177674e04488d474fe0c0fdde916dd2205ea9bbf
Joao Victor <jov9051@outlook.com>
2022-06-01 21:18:07 -0300

    refactor: productId adicionado a validateHandleEditProduct em validateProductData.ts

--- src/validators/validateProductData.ts
+++ src/validators/validateProductData.ts
@@ -15,7 +15,8 @@ const validateHandleEditProduct = (data: Object): Joi.ValidationResult => {
     title: Joi.string().required().min(3).max(150),
     description: Joi.string().required().min(5).max(150),
     amount: Joi.number().required().min(1),
-    price: Joi.number().required().min(1)
+    price: Joi.number().required().min(1),
+    productId: Joi.string().required().min(1)
   });
 
   return schema.validate(data);
